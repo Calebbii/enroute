e8600a6697835c906a354139a68621336ec0657e
Calebbii <biicaleb316@gmail.com>
2022-12-21 15:28:48 +0300

    Adds fields to capture data in the dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,3 +1,5 @@
+import Button from 'react-bootstrap/Button';
+
 function Dashboard() {
     return (
         <div className="dashboard-container">
@@ -463,8 +465,8 @@ function Dashboard() {
                                     </svg>
                                 </a>
                             </li>
-                            <li class="breadcrumb-item"><a href="#">Tables</a></li>
-                            <li class="breadcrumb-item active" aria-current="page">Bootstrap tables</li>
+                            <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Active Members</li>
                         </ol>
                         </nav>
                         <div class="d-flex justify-content-between w-100 flex-wrap">
@@ -529,6 +531,12 @@ function Dashboard() {
                                             <span class="fw-bold">2.45%</span>
                                         </div>
                                     </td>
+                                    <td>
+                                        <Button variant="warning" size="sm">Edit</Button>{' '}
+                                    </td>
+                                    <td>
+                                        <Button variant="danger" size="sm">Delete</Button>{' '}
+                                    </td>
                                     </tr>
                                     <tr>
                                     <td><a href="#" class="text-primary fw-bold">2</a></td>
@@ -561,6 +569,14 @@ function Dashboard() {
                                             <span class="fw-bold">17.78%</span>
                                         </div>
                                     </td>
+
+                                    <td>
+                                        <Button variant="warning" size="sm">Edit</Button>{' '}
+                                    </td>
+                                    <td>
+                                        <Button variant="danger" size="sm">Delete</Button>{' '}
+                                    </td>
+
                                     </tr>
                                     <tr>
                                     <td><a href="#" class="text-primary fw-bold">3</a></td>
@@ -877,19 +893,6 @@ function Dashboard() {
 
             <div class="row">
                 <div class="col-12 mb-4">
-                <div class="card bg-yellow-100 border-0 shadow">
-                    <div class="card-header d-sm-flex flex-row align-items-center flex-0">
-                        <div class="d-block mb-3 mb-sm-0">
-                            <div class="fs-5 fw-normal mb-2">Sales Value</div>
-                            <h2 class="fs-3 fw-extrabold">$10,567</h2>
-                            <div class="small mt-2"><span class="fw-normal me-2">Yesterday</span> <span class="fas fa-angle-up text-success"></span> <span class="text-success fw-bold">10.57%</span></div>
-                        </div>
-                        <div class="d-flex ms-auto"><a href="#" class="btn btn-secondary text-dark btn-sm me-2">Month</a> <a href="#" class="btn btn-dark btn-sm me-3">Week</a></div>
-                    </div>
-                    <div class="card-body p-2">
-                        <div class="ct-chart-sales-value ct-double-octave ct-series-g"></div>
-                    </div>
-                </div>
                 </div>
                 <div class="col-12 col-sm-6 col-xl-4 mb-4">
                 <div class="card border-0 shadow">
@@ -1094,7 +1097,7 @@ function Dashboard() {
                             </div>
                         </div>
                     </div>
-                    <div class="col-12 col-xxl-6 mb-4">
+                    {/* <div class="col-12 col-xxl-6 mb-4">
                         <div class="card border-0 shadow">
                             <div class="card-header border-bottom d-flex align-items-center justify-content-between">
                             <h2 class="fs-5 fw-bold mb-0">Team members</h2>
@@ -1185,8 +1188,8 @@ function Dashboard() {
                             </ul>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-12 col-xxl-6 mb-4">
+                    </div> */}
+                    {/* <div class="col-12 col-xxl-6 mb-4">
                         <div class="card border-0 shadow">
                             <div class="card-header border-bottom d-flex align-items-center justify-content-between">
                             <h2 class="fs-5 fw-bold mb-0">Progress track</h2>
@@ -1271,7 +1274,7 @@ function Dashboard() {
                             </div>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
                 </div>
                 <div class="col-12 col-xl-4">
