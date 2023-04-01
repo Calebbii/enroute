bd7077e01499969ffafd94efabafc5356bab3861
Calebbii <biicaleb316@gmail.com>
2023-04-01 16:06:22 +0300

    adds hooks to the signup page

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,4 +1,22 @@
+import React, { useState } from "react";
+import { auth } from "../firebase"
+import { signInWithEmailAndPassword } from "firebase/auth"
+
 function SignIn() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const signIn = (e)=> {
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredentials) => {
+            console.log(userCredentials)
+        }).catch((error) => {
+            console.log(error)
+        })
+    }
+
     return (
         <div className="signin-container">
           <section class="vh-lg-100 mt-5 mt-lg-0 bg-soft d-flex align-items-center">
@@ -9,37 +27,54 @@ function SignIn() {
                             <div class="text-center text-md-center mb-4 mt-md-0">
                             <h1 class="mb-0 h3">Sign in to our platform</h1>
                             </div>
-                            <form action="#" class="mt-4">
-                            <div class="form-group mb-4">
-                                <label for="email">Your Email</label>
-                                <div class="input-group">
-                                    <span class="input-group-text" id="basic-addon1">
-                                        <svg class="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
-                                        <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
-                                        </svg>
-                                    </span>
-                                    <input type="email" class="form-control" placeholder="example@company.com" id="email" autofocus required />
-                                </div>
-                            </div>
-                            <div class="form-group">
+                            <form onSubmit={signIn} action="#" class="mt-4">
                                 <div class="form-group mb-4">
-                                    <label for="password">Your Password</label>
+                                    <label for="email">Your Email</label>
                                     <div class="input-group">
-                                        <span class="input-group-text" id="basic-addon2">
-                                        <svg class="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                            <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd"></path>
-                                        </svg>
+                                        <span class="input-group-text" id="basic-addon1">
+                                            <svg class="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
+                                            <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
+                                            </svg>
                                         </span>
-                                        <input type="password" placeholder="Password" class="form-control" id="password" required />
+                                        <input 
+                                            type="email" 
+                                            class="form-control" 
+                                            placeholder="example@company.com" 
+                                            value={email}
+                                            onChange= {(e) => setEmail(e.target.value)}
+                                            id="email" 
+                                            autofocus required 
+                                        />
                                     </div>
                                 </div>
-                                <div class="d-flex justify-content-between align-items-top mb-4">
-                                    <div class="form-check"><input class="form-check-input" type="checkbox" value="" id="remember" /> <label class="form-check-label mb-0" for="remember">Remember me</label></div>
-                                    <div><a href="#" class="small text-right">Lost password?</a></div>
+                                <div class="form-group">
+                                    <div class="form-group mb-4">
+                                        <label for="password">Your Password</label>
+                                        <div class="input-group">
+                                            <span class="input-group-text" id="basic-addon2">
+                                            <svg class="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd"></path>
+                                            </svg>
+                                            </span>
+                                            <input 
+                                                type="password" 
+                                                placeholder="Password" 
+                                                value={password}
+                                                onChange= {(e) => setPassword(e.target.value)}
+                                                class="form-control" 
+                                                id="password" 
+                                            required />
+                                        </div>
+                                    </div>
+                                    <div class="d-flex justify-content-between align-items-top mb-4">
+                                        <div class="form-check"><input class="form-check-input" type="checkbox" value="" id="remember" /> <label class="form-check-label mb-0" for="remember">Remember me</label></div>
+                                        <div><a href="#" class="small text-right">Lost password?</a></div>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="d-grid"><button type="submit" class="btn btn-gray-800">Sign in</button></div>
+                                <div class="d-grid">
+                                    <button type="submit" class="btn btn-gray-800">Sign in</button>
+                                </div>
                             </form>
                             <div class="mt-3 mb-4 text-center"><span class="fw-normal">or login with</span></div>
                             <div class="d-flex justify-content-center my-4">
